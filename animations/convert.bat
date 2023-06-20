f6652409975c896651ce1832abff108ddadc665b
Janos Wortmann <jwortmann@outlook.com>
2023-06-20 22:52:13 +0200

    Add new setting for fold buttons style

--- animations/convert.bat
+++ animations/convert.bat
@@ -22,3 +22,27 @@ inkscape --export-type=png -w 48 -h 48 --export-filename=fold5@3x.png fold5.svg
 inkscape --export-type=png -w 16 -h 16 --export-filename=fold6.png fold6.svg
 inkscape --export-type=png -w 32 -h 32 --export-filename=fold6@2x.png fold6.svg
 inkscape --export-type=png -w 48 -h 48 --export-filename=fold6@3x.png fold6.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold_closed_square.png fold_closed_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold_closed_square@2x.png fold_closed_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold_closed_square@3x.png fold_closed_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold_opened_square.png fold_opened_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold_opened_square@2x.png fold_opened_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold_opened_square@3x.png fold_opened_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold1_square.png fold1_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold1_square@2x.png fold1_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold1_square@3x.png fold1_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold2_square.png fold2_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold2_square@2x.png fold2_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold2_square@3x.png fold2_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold3_square.png fold3_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold3_square@2x.png fold3_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold3_square@3x.png fold3_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold4_square.png fold4_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold4_square@2x.png fold4_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold4_square@3x.png fold4_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold5_square.png fold5_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold5_square@2x.png fold5_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold5_square@3x.png fold5_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold6_square.png fold6_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold6_square@2x.png fold6_square.svg
+inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold6_square@3x.png fold6_square.svg
