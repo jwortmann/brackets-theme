3893df910d2dece6856a283f72f733ed5484a240
Janos Wortmann <jwortmann@outlook.com>
2023-06-21 20:40:15 +0200

    Add animation for modified tab close button hover

--- assets/convert.bat
+++ assets/convert.bat
@@ -1,3 +1,6 @@
+inkscape --export-type=png -w 16 -h 16 --export-filename=annotation_close.png annotation_close.svg
+inkscape --export-type=png -w 32 -h 32 --export-filename=annotation_close@2x.png annotation_close.svg
+inkscape --export-type=png -w 48 -h 48 --export-filename=annotation_close@3x.png annotation_close.svg
 inkscape --export-type=png -w 16 -h 16 --export-filename=branch.png branch.svg
 inkscape --export-type=png -w 32 -h 32 --export-filename=branch@2x.png branch.svg
 inkscape --export-type=png -w 48 -h 48 --export-filename=branch@3x.png branch.svg
