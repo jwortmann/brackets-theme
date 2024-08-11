92b2d72367c0f5e5d80c160cf52e2ed9f01838a2
Janos Wortmann <jwortmann@outlook.com>
2024-08-12 01:46:54 +0200

    Move SVG files

--- svg/convert.bat
+++ svg/convert.bat
@@ -436,3 +436,52 @@ rem inkscape --export-type=png --export-dpi=288 --export-filename=tab_wave_highl
 rem inkscape --export-type=png --export-dpi=96 --export-filename=tab_wave_divider.png tab_wave_divider.svg
 rem inkscape --export-type=png --export-dpi=192 --export-filename=tab_wave_divider@2x.png tab_wave_divider.svg
 rem inkscape --export-type=png --export-dpi=288 --export-filename=tab_wave_divider@3x.png tab_wave_divider.svg
+
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold_closed.png fold_closed.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold_closed@2x.png fold_closed.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold_closed@3x.png fold_closed.svg
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold_opened.png fold_opened.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold_opened@2x.png fold_opened.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold_opened@3x.png fold_opened.svg
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold1.png fold1.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold1@2x.png fold1.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold1@3x.png fold1.svg
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold2.png fold2.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold2@2x.png fold2.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold2@3x.png fold2.svg
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold3.png fold3.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold3@2x.png fold3.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold3@3x.png fold3.svg
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold4.png fold4.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold4@2x.png fold4.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold4@3x.png fold4.svg
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold5.png fold5.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold5@2x.png fold5.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold5@3x.png fold5.svg
+rem inkscape --export-type=png -w 16 -h 16 --export-filename=fold6.png fold6.svg
+rem inkscape --export-type=png -w 32 -h 32 --export-filename=fold6@2x.png fold6.svg
+rem inkscape --export-type=png -w 48 -h 48 --export-filename=fold6@3x.png fold6.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold_closed_square.png fold_closed_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold_closed_square@2x.png fold_closed_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold_closed_square@3x.png fold_closed_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold_opened_square.png fold_opened_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold_opened_square@2x.png fold_opened_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold_opened_square@3x.png fold_opened_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold1_square.png fold1_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold1_square@2x.png fold1_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold1_square@3x.png fold1_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold2_square.png fold2_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold2_square@2x.png fold2_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold2_square@3x.png fold2_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold3_square.png fold3_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold3_square@2x.png fold3_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold3_square@3x.png fold3_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold4_square.png fold4_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold4_square@2x.png fold4_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold4_square@3x.png fold4_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold5_square.png fold5_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold5_square@2x.png fold5_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold5_square@3x.png fold5_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 16 -h 16 --export-filename=fold6_square.png fold6_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 32 -h 32 --export-filename=fold6_square@2x.png fold6_square.svg
+rem inkscape --export-type=png --export-area=-4.091:-2.818:16.273:17.546 -w 48 -h 48 --export-filename=fold6_square@3x.png fold6_square.svg
